f423a72ddad542042a688bd9383db8eb20799e01
Sebastián <seba.chavez21@gmail.com>
2023-11-26 13:32:12 -0300

    upd validaciones

--- src/routers/file.js
+++ src/routers/file.js
@@ -16,6 +16,7 @@ router.post('/update-file/:appId',
     ],
     (req, res, next) => {
         const errors = validationResult(req)
+        console.log({errors});
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() })
         }
