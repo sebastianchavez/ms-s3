5b8276d00845bf3216ef31f6649ed544a65b4831
Sebastián <seba.chavez21@gmail.com>
2023-11-26 13:33:20 -0300

    checks

--- src/routers/file.js
+++ src/routers/file.js
@@ -6,22 +6,22 @@ const isAuth = require('../middlewares/auth')
 
 router.post('/update-file/:appId',
     isAuth,
-    [
-        check('file', 'Campo file es requerido').exists(),
-        check('file', 'Campo file debe ser un campo base64').isBase64(),
-        check('name', 'Campo name es requerido').exists(),
-        check('name', 'Campo name debe ser un campo string').isString(),
-        check('path', 'Campo path es requerido').exists(),
-        check('path', 'Campo path debe ser un campo string').isString(),
-    ],
-    (req, res, next) => {
-        const errors = validationResult(req)
-        console.log({errors});
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() })
-        }
-        next()
-    },
+    // [
+    //     check('file', 'Campo file es requerido').exists(),
+    //     check('file', 'Campo file debe ser un campo base64').isBase64(),
+    //     check('name', 'Campo name es requerido').exists(),
+    //     check('name', 'Campo name debe ser un campo string').isString(),
+    //     check('path', 'Campo path es requerido').exists(),
+    //     check('path', 'Campo path debe ser un campo string').isString(),
+    // ],
+    // (req, res, next) => {
+    //     const errors = validationResult(req)
+    //     console.log({errors});
+    //     if (!errors.isEmpty()) {
+    //         return res.status(400).json({ errors: errors.array() })
+    //     }
+    //     next()
+    // },
     fileCtrl.updateFile
 )
 
